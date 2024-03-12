cf0e784c7d3717402e8557d1e651bc68907ca7a2
geogeolo <163122629+geogeolo@users.noreply.github.com>
2024-03-12 23:49:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def handle_message(event):
         messages=[
             {"role": "user", "content": text1}
         ],
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo-0125",
         temperature = 0.5,
     )
     try:
